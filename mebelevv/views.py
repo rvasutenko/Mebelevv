575dc3eb33256d0b530c4fa57f40a5295a16ba3d
Your Name <you@example.com>
2024-09-06 01:04:45 +0300

    add contact window + Dockerfile

--- mebelevv/views.py
+++ mebelevv/views.py
@@ -33,15 +33,19 @@ class MainPage(View):
 
     def post(self, request):
         answers = {}
-        for q in Question.objects.all():
-            answers[request.POST.get(q.title).split(' // ')[0]] = request.POST.get(q.title).split(' // ')[1]
         phoneNumber = request.POST.get('phoneNumber')
         filename = None
-        if request.method == 'POST' and request.FILES:
-            file = request.FILES['userFile']
-            fs = FileSystemStorage()
-            filename = fs.save(file.name, file)
+        try:
+            for q in Question.objects.all():
+                answers[request.POST.get(q.title).split(' // ')[0]] = request.POST.get(q.title).split(' // ')[1]
+            if request.method == 'POST' and request.FILES:
+                file = request.FILES['userFile']
+                fs = FileSystemStorage()
+                filename = fs.save(file.name, file)
+        except:
+            answers['Тип'] = 'Заявка на звонок'
         UserInfo(answers=answers, phoneNumber=phoneNumber, img=filename).save()
+
 
         questions = Question.objects.all()
         gallery = Gallery.objects.all()
